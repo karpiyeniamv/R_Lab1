ecdd674743c347fe109aead611bf77323f47d3d4
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-10-03 15:33:09 +0300

    without task5

--- lab1.R
+++ lab1.R
@@ -1,5 +1,7 @@
 # 13 Мария Карпиеня
 
+#task 1
+
 len <- nchar('Карпиеня')
 num <- 13
 
@@ -15,12 +17,81 @@ v1
 # геометрическая
 len <- nchar('Мария')
 x <- (13 - num)+2
+x
 q = (100/x)  ^ (1/(len-1))
 q
-
 v <- c(0:4)
 v2 <- c()
-for (i in v) {
-  v2[i+1] = x * (d ** i)
+geomSeq <- function(start,ratio,begin,end){
+  begin=begin-1
+  end=end-1
+ return (start*ratio**(begin:end))
 }
+v2 = geomSeq(x, q, 1, 5)
 v2
+
+
+v3 <- sample(c(v1,v2)) 
+v3
+
+
+#task2 
+alf <- 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+v_alf <-c()
+v <- c(1:33)
+for (i in v) {
+  v_alf[i] = substr (alf,i,i)
+}
+v_alf
+str_sum <- function(s){
+  res <- 0
+  len <- nchar(s)
+  v <- c(1:len)
+  for (i in v) {
+    res = res + charmatch(substr (s,i,i), v_alf, 0)
+  }
+  return (res)
+}
+str_sum ('абвг')
+
+#task3
+v11 <- c(v1,v1[1],v1[2],v1[3],v1[4],v1[5])
+v3[1]
+if (v3[1]>10)
+{
+   m <- t (matrix(v11, ncol = 13, nrow=13))
+}else
+{
+  m <- matrix(v11, ncol = 13, nrow=13)
+}
+#month.name
+rownames(m) <- c (month.name,"January")
+colnames(m) <- c (month.name,"January")
+m <- m[-c(2,4,8),]
+m <- m[, -c(2,4,8)]
+m
+det (m)
+diag(m)
+eigen(m)
+v12 <- v1%*%t(v1)
+v12
+v2 <- v12^2
+v2
+
+
+#task4
+v <- c(1,0,12,0,0,8,0,2)
+str <- c('nu','m','ni','hao','wo','she','by','losy')
+flag <- c (T,F,F,F,F,T,T,F)
+we <- c(1,0,2,1,1,2,1,0)
+we.f <- factor(we, levels=0:2)
+we.f
+tab <- data.frame (text = str, usl = flag, num = v, fact = we)
+tab
+tab [tab$num > tab$fact, 1 ]
+
+#task5
+
+
+
+
